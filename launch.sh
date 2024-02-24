32c322df5c212a95c5d0c24349b34b9b4853afa6
Claudio Gagliardi <claudio.gagliardi@gmail.com>
2024-02-24 19:53:32 +0100

    up

--- launch.sh
+++ launch.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 docker-compose up -d  nginx redis workspace php-fpm php-worker
+docker-compose exec workspace bash
+chown -R laradock:laradock /var/www
 docker exec -it --user=laradock laradock-workspace-1 php artisan storage:link 
-docker exec -it --user=laradock laradock-workspace-1 composer install && npm i && npm run dev
+docker exec -it --user=laradock laradock-workspace-1 composer install 
+docker exec -it --user=laradock laradock-workspace-1 npm i 
+docker exec -it --user=laradock laradock-workspace-1 npm run dev
